c1011d4e62c7eefe369c121666aa7a4f25405149
Vishal Singh <iamvishal.2811@gmail.com>
2025-11-03 01:07:54 +0530

    changes in init

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,6 @@ from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
 import os
 load_dotenv()
-from fastapi import FastAPI
-aws_access_key_id =os.getenv("aws_access_key_id")
-aws_secret_access_key =os.getenv("aws_secret_access_key")
-region = os.getenv("region")
+
 table_name = os.getenv("table_name")
-__all__ = ["app","BaseModel","APIRouter","HTTPException","aws_access_key_id","aws_secret_access_key","region","table_name"]   
+__all__ = ["app","BaseModel","APIRouter","HTTPException","table_name"]   
